9d991219d4fce7ad67f440c4efdfe59c80005dfb
artur <artur@artur-PC>
2017-01-06 15:45:14 +0100

    1.12

--- MySwingBMS/src/swingBms/com/FTPconnection.java
+++ MySwingBMS/src/swingBms/com/FTPconnection.java
@@ -6,7 +6,7 @@ import java.net.SocketException;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
+
 import org.apache.commons.net.ftp.FTPReply;
 
 public class FTPconnection {
